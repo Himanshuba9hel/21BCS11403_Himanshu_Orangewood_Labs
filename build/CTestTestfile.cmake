fdb018ddc26c477efeaf007c46aec6827418b420
Himanshuba9hel <himanshuba9hel@gmail.com>
2024-11-11 13:54:45 +0530

    ALL

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/himanshu_21bcs11403/ros_ws/src
+# Build directory: /home/himanshu_21bcs11403/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros_session")
